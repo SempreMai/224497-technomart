4e32996bc001f1babe83c5a32e202701e75750f0
Liubov <lyuba.kudrina@gmail.com>
2018-04-23 14:32:35 +0200

    Исправила ошибки первой проверки

--- js/script.js
+++ js/script.js
@@ -16,7 +16,22 @@ var nameWrite = formWrite.querySelector("[name = your_name]");
 
 var emailWrite = formWrite.querySelector("[name = your_email]");
 
-var fillForm = formWrite.querySelector("form");
+var fillForm = modalWrite.querySelector("form");
+
+var storage = localStorage.getItem("emailWrite");
+
+var isStorageSupport = true;
+var storage = "";
+
+try {
+
+  storage = localStorage.getItem("nameWrite");
+
+} catch (err) {
+
+  isStorageSupport = false;
+
+}
 
 writeUs.addEventListener("click", function (evt) {
 
@@ -25,6 +40,12 @@ writeUs.addEventListener("click", function (evt) {
    modalWrite.classList.add("modal-show");
 
    nameWrite.focus();
+
+   if (storage) {
+
+     nameWrite.value = storage;
+
+   }
 });
 
 closeWriteUs.addEventListener("click", function (evt) {
@@ -32,6 +53,8 @@ closeWriteUs.addEventListener("click", function (evt) {
   evt.preventDefault();
 
   modalWrite.classList.remove("modal-show");
+
+  modalWrite.classList.remove("modal-error");
 });
 
 fillForm.addEventListener("submit", function (evt) {
@@ -40,12 +63,13 @@ fillForm.addEventListener("submit", function (evt) {
 
     evt.preventDefault();
 
-    console.log("Нужно ввести имя и адрес");
+    modalWrite.classList.add("modal-error");
 
   } else {
 
     if (isStorageSupport) {
-      localStorage.setItem("your_email", emailWrite.value);
+
+      localStorage.setItem("your_name", nameWrite.value);
     }
   }
 });
@@ -62,4 +86,32 @@ mapClose.addEventListener("click", function (evt) {
   evt.preventDefault();
 
   mapPopup.classList.remove("modal-show");
-})
+});
+
+window.addEventListener("keydown", function (evt) {
+
+  if (evt.keyCode === 27) {
+
+    evt.preventDefault();
+
+    if (modalWrite.classList.contains("modal-show")) {
+
+      modalWrite.classList.remove("modal-show");
+
+    }
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+
+  if (evt.keyCode === 27) {
+
+    evt.preventDefault();
+
+    if (mapPopup.classList.contains("modal-show")) {
+
+      modalWrite.classList.remove("modal-show");
+
+    }
+  }
+});
